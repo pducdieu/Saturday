c71ef59070dbf5f287747a40f63206bdd8220567
pducdieu <pducdieu17102002@gmail.com>
2024-12-23 21:11:00 +0700

    do an tot nghiep

---
+++ Desktop/c#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Nhập hai số từ người dùng
+        Console.Write("Nhập số thứ nhất: ");
+        int soThuNhat = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nhập số thứ hai: ");
+        int soThuHai = Convert.ToInt32(Console.ReadLine());
+
+        // Tính tổng của hai số
+        int tong = soThuNhat + soThuHai;
+
+        // Lấy dư khi chia tổng cho 10
+        int du = tong % 10;
+
+        // In kết quả ra màn hình
+        Console.WriteLine("Tổng của hai số là: " + tong);
+        Console.WriteLine("Dư khi chia tổng cho 10 là: " + du);
+
+        Console.ReadLine(); // Để chương trình không đóng ngay sau khi in kết quả
+    }
+
+   
+} 
